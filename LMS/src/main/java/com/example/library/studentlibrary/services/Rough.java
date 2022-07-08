0d6f0b5155e954775c668e93a0f8097eb705d452
Hardik Negi <78537057+hardiknegi98@users.noreply.github.com>
2022-07-08 14:44:14 +0530

    Add files via upload

---
+++ LMS/src/main/java/com/example/library/studentlibrary/services/Rough.java
@@ -0,0 +1,14 @@
+package com.example.library.studentlibrary.services;
+
+public class Rough {
+
+    public void returnBook (int bookID , int cardID){
+        /*
+        get transaction with
+
+
+        */
+
+
+    }
+}
